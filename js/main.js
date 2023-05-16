cc16a79b45327b7ff28584da0e9f52ee4f3e4420
mdridoy-khan <mdridoy9902@gmail.com>
2023-05-16 09:39:43 +0600

    first-commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 (function ($) {
 "use strict";
 
+
+ /*---------------------------------------------------------------------------------------
+    Magnific popup Active
+-----------------------------------------------------------------------------------------*/
+    // video popup
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+    });
+
+
 /*---------------------------------------------------------------------------------------
     Mean Menu Active
 -----------------------------------------------------------------------------------------*/
@@ -8,6 +18,7 @@
     $('#mobile-menu').meanmenu({
         meanMenuContainer: '.mobile-menu',
         meanScreenWidth: "767",
+        meanRevealPositionDistance: "0",
     });
 /*---------------------------------------------------------------------------------------
     Data Background Active
@@ -27,315 +38,107 @@
           $("#header-sticky").addClass("sticky");
         }
     }); 
-    // sticky Header layout 2, 3
-    $(window).on('scroll', function () {
-        var scroll = $(window).scrollTop();
-        if (scroll < 5) {
-          $("#header-sticky-2").removeClass("sticky");
-        } else {
-          $("#header-sticky-2").addClass("sticky");
-        }
-    }); 
-    // sticky Header Inner page
-    $(window).on('scroll', function () {
-        var scroll = $(window).scrollTop();
-        if (scroll < 5) {
-          $("#header-sticky-3").removeClass("sticky");
-        } else {
-          $("#header-sticky-3").addClass("sticky");
-        }
-    });
 	
+
+
+
 /*---------------------------------------------------------------------------------------
-    Full Screen Search Popup Active
------------------------------------------------------------------------------------------*/                                     
-    // search popup
-    if ($('.popup-with-zoom-anim').length) {
-        $('.popup-with-zoom-anim').magnificPopup({
-            type: 'inline',
-
-            fixedContentPos: false,
-            fixedBgPos: true,
-
-            overflowY: 'auto',
-
-            closeBtnInside: true,
-            preloader: false,
-
-            midClick: true,
-            removalDelay: 300,
-            mainClass: 'my-mfp-zoom-in'
-        });
-    }
-
-
-    // owlCarousel slider protfolio-slider active
-    $('.protfolio-slider').owlCarousel({
-        animateOut: 'animate__zoomOut',
-        animateIn: 'animate__fadeInDown',
-        dots:false,
-        autoplay:true,
-        loop:true,
-        navText:['<i class="fa-solid fa-left-long"></i>','<i class="fa-solid fa-right-long"></i>'],
-        nav:true,
-        autoplay:true,
-        items:1,
-        margin:0,
-        stagePadding:0,
-        smartSpeed:450
-    });
-/*---------------------------------------------------------------------------------------
-    Slick Slider Active
+    slider all section
 -----------------------------------------------------------------------------------------*/  
 
-
-// mainSlider
-    function mainSlider() {
-        var BasicSlider = $('.slider-active');
-        BasicSlider.on('init', function (e, slick) {
-            var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
-            doAnimations($firstAnimatingElements);
-        });
-        BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
-            var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
-            doAnimations($animatingElements);
-        });
-        BasicSlider.slick({
-            autoplay: true,
-            autoplaySpeed: 1000,
-            dots: false,
-            fade: true,
-            arrows: false,
-            pauseOnHover:false,
-            prevArrow:'<i class="fas fa-chevron-left"></i>',
-            nextArrow:'<i class="fas fa-chevron-right"></i>',
-            responsive: [
-            {
-              breakpoint: 993,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                arrows: false,
-              }
+    // main slider area
+    $('.slider-active-off').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:false,
+        dots:true,
+        navText:['<i class="fa-solid fa-left-long"></i>','<i class="fa-solid fa-right-long"></i>'],
+        responsive:{
+            0:{
+                items:1
             },
-            {
-              breakpoint: 767,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                arrows: false,
-                dots: true,
-              }
+            600:{
+                items:1
             },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                arrows: false,
-                dots: true,
-              }
+            1200:{
+                items:1
+            },
+            1300:{
+                items:1
             }
-          ]
-        });
-
-        function doAnimations(elements) {
-            var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-            elements.each(function () {
-                var $this = $(this);
-                var $animationDelay = $this.data('delay');
-                var $animationType = 'animated ' + $this.data('animation');
-                $this.css({
-                    'animation-delay': $animationDelay,
-                    '-webkit-animation-delay': $animationDelay
-                });
-                $this.addClass($animationType).one(animationEndEvents, function () {
-                    $this.removeClass($animationType);
-                });
-            });
         }
-    }
-    mainSlider();
-	
-    // skill area slider active
-    $('.skill-activ').slick({
-        autoplay: true,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-        arrows: false,
-        autoplaySpeed: 1000,
-        prevArrow:'<i class="fas fa-chevron-left"></i>',
-        nextArrow:'<i class="fas fa-chevron-right"></i>',
-        responsive: [
-        {
-          breakpoint: 993,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-          }
-        }
-      ]
-    });
-	
-	// skill area slider active
-	$('.skill-activation').owlCarousel({
-		loop:true,
-		margin:10,
-		nav:false,
-		dots:true,
-		responsive:{
-			0:{
-				items:1
-			},
-			600:{
-				items:1
-			},
-			1000:{
-				items:1
-			}
-		}
-	})
-	
-	
-	// skill area slider active
-	
-    // Blog post slider active
-    $('.latest-blog-active').slick({
-        autoplay: true,
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        dots: false,
-        arrows: true,
-        autoplaySpeed: 1000,
-        prevArrow:'<i class="fas fa-chevron-left"></i>',
-        nextArrow:'<i class="fas fa-chevron-right"></i>',
-    });
+    })
 
-    // brand slider active
-    $('.brand-active').slick({
-        autoplay: true,
-        infinite: true,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        dots: false,
-        arrows: false,
-        autoplaySpeed: 1000,
-        responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            infinite: true,
-          }
-        },
-        {
-          breakpoint: 993,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 540,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 400,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
+    // service area slider 
+      $('.service-slider-wrapper').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        navText:['<span><img src="img/services/Vector.svg"></span>','<span><img src="img/services/Vector1.svg"></span>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1200:{
+              items:4
+            },
+            1300:{
+                items:4
+            }
         }
-      ]
-    });
+    })
 
-    // author-review active
-    $('.author-review-active').slick({
-        autoplay: true,
-        infinite: true,
-        dots: true,
-        arrows: false,
-        autoplaySpeed: 10000,
-    });
 
-    // author-review active
-    $('.testimonial-active').slick({
-        autoplay: false,
-        infinite: true,
-        dots: false,
-        arrows: true,
-        autoplaySpeed: 10000,
-        prevArrow:'<i class="fas fa-chevron-left"></i>',
-        nextArrow:'<i class="fas fa-chevron-right"></i>',
-        responsive: [
-        {
-          breakpoint: 767,
-          settings: {
-            infinite: true,
-            arrows: false,
-          }
+    // latest-blog area slider 
+    $('.latest-blog-wrapper').owlCarousel({
+      loop:true,
+      margin:30,
+      nav:false,
+      dots:true,
+      navText:['<i class="fa-solid fa-left-long"></i>','<i class="fa-solid fa-right-long"></i>'],
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+            items:4
         },
-      ]
-    });
-/*---------------------------------------------------------------------------------------
-    Barfiller Progress Bar Active
------------------------------------------------------------------------------------------*/
-    $(window).on('scroll', function () {
-        var scroll = $(window).scrollTop();
-        if (scroll > 850) {
-            $('#bar1').barfiller();
-            $('#bar2').barfiller();
-            $('#bar3').barfiller();
-            $('#bar4').barfiller();
-        }
-    });
-/*---------------------------------------------------------------------------------------
-    Isotope Filter Active
------------------------------------------------------------------------------------------*/
-    // Latest work isotope active
-    var $grid = $('.latest-work-items').isotope({
-      itemSelector: '.grid-item',
-      percentPosition: true,
-      masonry: {
-        // use outer width of grid-sizer for columnWidth
-        columnWidth: '.grid-item',
+          1300:{
+              items:4
+          }
       }
-    });
+    })
 
-    // filter items on button click
-    $('.work-control-button').on( 'click', 'button', function() {
-      var filterValue = $(this).attr('data-filter');
-      $grid.isotope({ filter: filterValue });
-    });
-    //for button active class
-    $('.work-control-button > button').on('click', function(event) {
-        $(this).siblings('.active').removeClass('active');
-        $(this).addClass('active');
-        event.preventDefault();
-    });
-/*---------------------------------------------------------------------------------------
-    Magnific popup Active
------------------------------------------------------------------------------------------*/
-    // image popup
-    $('.popup-pic').magnificPopup({
-        type: 'image',
-        gallery:{
-            enabled:true
+
+
+    // team area slider 
+    $('.team-wrapper').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:false,
+        dots:true,
+        navText:['<i class="fa-solid fa-left-long"></i>','<i class="fa-solid fa-right-long"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1300:{
+                items:1
+            }
         }
-    });
-    // video popup
-    $('.popup-video').magnificPopup({
-        type: 'iframe'
-    });
+    })
+
+
+ 
 /*---------------------------------------------------------------------------------------
     Scroll to top Active
 -----------------------------------------------------------------------------------------*/
@@ -349,148 +152,25 @@
         scrollText: '<span><i class="fa-solid fa-arrow-up"></i></span>', // Text for element
         activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
     });
-/*---------------------------------------------------------------------------------------
-    AOS Animation Active
------------------------------------------------------------------------------------------*/
-    if ($("[data-aos]").length) {
-        AOS.init({
-            duration: 1000,
-            mirror: true
-        });
-    }
-/*---------------------------------------------------------------------------------------
-    Wow Animation Active
------------------------------------------------------------------------------------------*/ 
 
-    var wow = new WOW(
-  {
-    boxClass:     'wow',      // animated element css class (default is wow)
-    animateClass: 'animated', // animation css class (default is animated)
-    offset:       0,          // distance to the element when triggering the animation (default is 0)
-    mobile:       true,       // trigger animations on mobile devices (default is true)
-    live:         true,       // act on asynchronously loaded content (default is true)
-    callback:     function(box) {
-      // the callback is fired every time an animation is started
-      // the argument that is passed in is the DOM node being animated
-    },
-    scrollContainer: null,    // optional scroll container selector, otherwise use window,
-    resetAnimation: true,     // reset animation on end (default is true)
-  }
-);
-wow.init();
-/*---------------------------------------------------------------------------------------
-    Counter Active
------------------------------------------------------------------------------------------*/
-    $('.counter').counterUp({
-        delay: 10,
-        time: 1000
+
+    // one page nav
+    $('#one-page-nav').onePageNav({
+        changeHash: false,
+        scrollSpeed: 750,
+        scrollThreshold: 0.5,
+        filter: '',
+        easing: 'swing',
+        begin: function() {
+        //I get fired when the animation is starting
+        },
+        end: function() {
+        //I get fired when the animation is ending
+        },
+        scrollChange: function($currentListItem) {
+        //I get fired when you enter a section and I pass the list item of the section
+        }
     });
-
-
-
-
-
-/*---------------------------------------------------------------------------------------
-    Preloader Active
------------------------------------------------------------------------------------------*/
-    function loader() {
-        $(window).on('load', function () {
-            $('#ctn-preloader').addClass('loaded');
-            $("#loading").fadeOut(500);
-            // Una vez haya terminado el preloader aparezca el scroll
-
-            if ($('#ctn-preloader').hasClass('loaded')) {
-                // Es para que una vez que se haya ido el preloader se elimine toda la seccion preloader
-                $('#preloader').delay(900).queue(function () {
-                    $(this).remove();
-                });
-            }
-        });
-    }
-    loader();
-
-/*---------------------------------------------------------------------------------------
-    Side-bar Active
------------------------------------------------------------------------------------------*/
-    $('.bar').on("click", function() {
-        $('.btn-menu-main').addClass('btn-menu-main-right');
-    });
-    $('.crose').on("click", function() {
-        $('.btn-menu-main').removeClass('btn-menu-main-right');
-    });
-
-
-
-/*---------------------------------------------------------------------------------------
-    Map Active
------------------------------------------------------------------------------------------*/ 
-// function basicmap() {
-//     // Basic options for a simple Google Map
-//     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-//     var mapOptions = {
-//         // How zoomed in you want the map to start at (always required)
-//         zoom: 11,
-//         scrollwheel: false,
-//         // The latitude and longitude to center the map (always required)
-//         center: new google.maps.LatLng(40.712776, -74.005974), // New York
-//         // This is where you would paste any style found on Snazzy Maps.
-//         styles: [{ "featureType": "landscape", "elementType": "all", "stylers": [{ "visibility": "on" }] }, { "featureType": "poi.business", "elementType": "all", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "poi.business", "elementType": "labels", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "poi.park", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "poi.school", "elementType": "all", "stylers": [{ "visibility": "on" }] }, { "featureType": "poi.sports_complex", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit.station.bus", "elementType": "all", "stylers": [{ "visibility": "on" }, { "saturation": "21" }, { "weight": "4.05" }] }]
-//     };
-//     // Get the HTML DOM element that will contain your map
-//     // We are using a div with id="map" seen below in the <body>
-//     var mapElement = document.getElementById('contact-map');
-
-//     // Create the Google Map using our element and options defined above
-//     var map = new google.maps.Map(mapElement, mapOptions);
-
-//     // Let's also add a marker while we're at it
-//     var marker = new google.maps.Marker({
-//         position: new google.maps.LatLng(40.712776, -74.005974),
-//         map: map,
-//         title: 'Cryptox'
-//     });
-// }
-// if ($('#contact-map').length != 0) {
-//     google.maps.event.addDomListener(window, 'load', basicmap);
-// }
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: 23.8103, lng: 90.4125 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 8,
-      center: uluru,
-        flat: true,
-        styles: [ { "stylers": [ { "hue": "#4bd6bf" }, { "gamma": "1.58" } ] } ],
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-  }
-  
-  window.initMap = initMap;
-
-  // one page nav
-  $('#one-page-nav').onePageNav({
-    changeHash: false,
-    scrollSpeed: 750,
-    scrollThreshold: 0.5,
-    filter: '',
-    easing: 'swing',
-    begin: function() {
-      //I get fired when the animation is starting
-    },
-    end: function() {
-      //I get fired when the animation is ending
-    },
-    scrollChange: function($currentListItem) {
-      //I get fired when you enter a section and I pass the list item of the section
-    }
-  });
 
 
 })(jQuery); 
